7b5b782d8cee398c400168d1ffc9b21238e73a04
Anto Ivic <anto.ivic23@gmail.com>
2020-06-06 18:13:39 +0200

    Zadatak1+primjeri

---
+++ lv7nadoknada/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lv7nadoknada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
